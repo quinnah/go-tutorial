45cd17dcd0f5149fba96f29508668d1b88079707
Quinna Halim <quinnah@umich.edu>
2022-05-30 15:57:22 -0400

    Finished basic golang tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -16,9 +16,9 @@ func main() {
 
     // A slice of names.
     names := []string{"Gladys", "Samantha", "Darrin"}
-    
+
 	// Request a greeting message.
-    message, err := greetings.Hello("Quinna")
+    message, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
